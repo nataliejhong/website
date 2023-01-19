11974981446595acc774f97b207a240f9f8909c3
Natalie <nataliejhong@gmail.com>
2023-01-18 19:48:42 -0600

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// ============= MENU SHOW Y HIDDEN ================
+const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
+
+// ============= MENU SHOW ==============
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// ============= MENU HIDDEN ================
+
+
+
+// ============= REMOVE MENU MOBILE ================
+
+
+
+// ============= ACCORDION SKILLS ================
+
+
+
+// ============= QUALIFICATION TABS ================
+
+
+// ============= SERVICES MODAL ================
